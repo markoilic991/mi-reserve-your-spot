c4091b8f6acb69c316c5d2cd6c0903aa657c1ba3
Marko Ilic <marko.ilic@prodyna.com>
2021-12-21 13:22:46 +0100

    RYS Task5 add method findByOfficeSpaceId add new method inside OfficeRoomController, OfficeRoomService, and OfficeRoomRepository

--- src/main/java/com/prodyna/reserveyourspot/repository/OfficeRoomRepository.java
+++ src/main/java/com/prodyna/reserveyourspot/repository/OfficeRoomRepository.java
@@ -5,12 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface OfficeRoomRepository extends JpaRepository<OfficeRoom, Integer> {
-
   @Query("select o from OfficeRoom o where o.officeSpace.id= :id")
   public List<OfficeRoom> findByOfficeSpaceId(@Param("id") int officeSpaceId);
 }
