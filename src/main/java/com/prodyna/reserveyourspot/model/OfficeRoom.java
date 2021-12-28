01887cabca87ee3ab56fd13acbd91fc638fb172b
Marko Ilic <marko.ilic@prodyna.com>
2021-12-28 10:48:24 +0100

    RYS Task2 refactor OfficeSpace and OfficeRoom rewrite OfficeSpace and OfficeRoom

--- src/main/java/com/prodyna/reserveyourspot/model/OfficeRoom.java
+++ src/main/java/com/prodyna/reserveyourspot/model/OfficeRoom.java
@@ -1,39 +1,25 @@
 package com.prodyna.reserveyourspot.model;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 
-@Getter
-@Setter
-@ToString
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
 @Entity
 @Table(name = "officeRooms")
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class OfficeRoom {
 
   @Id
@@ -43,25 +29,13 @@ public class OfficeRoom {
   @NotNull(message = "OfficeRoom name must have a value!")
   private String name;
 
-  @NotNull(message = "OfficeRoom name must have a order number!")
-  @Min(1)
-  @Max(4)
-  private int orderNo;
+  @NotNull(message = "OfficeRoom name must have a code!")
+  @Column(unique = true)
+  private int code;
 
-  @ManyToOne(cascade = CascadeType.MERGE)
-  @JsonIgnoreProperties("rooms")
+  @ManyToOne
+  @JsonIgnore
   @JoinColumn(name = "officeSpace_Id")
   private OfficeSpace officeSpace;
-
-  public OfficeRoom(String name, int orderNo) {
-    this.name = name;
-    this.orderNo = orderNo;
-  }
-
-  public OfficeRoom(int id, String name, int orderNo) {
-    this.id = id;
-    this.name = name;
-    this.orderNo = orderNo;
-  }
 
 }
